69a74b747e0e6d51f8f68cf3696294127bac5052
DruidNoname <oboro.msu@gmail.com>
2021-05-30 20:51:44 +0300

    module2-task1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
 // источник - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
 
-function generateRandomNumber(minValue, maxValue){
-  if (minValue >= 0 && minValue < maxValue){
-    return Math.round(Math.random( ) * (maxValue - minValue)) + minValue;
+function generateRandomIntegerNumberFromInterval(minValue, maxValue) {
+  const RANGE_OF_NUMBERS = maxValue - minValue;
+  const INTEGER_RANGE_OF_NUMBERS = Math.floor(maxValue) - Math.ceil(minValue);
+  if (typeof minValue !== 'number' && typeof minValue !== 'number') {
+    return 'Аргументы функции должны быть числами';
+  } else if (RANGE_OF_NUMBERS <= 0) {
+    return 'Меньшее число должно быть меньше большего числа';
+  } else if (INTEGER_RANGE_OF_NUMBERS < 0) {
+    return 'В указанном интервале отсутствуют целые числа';
+  } else if (minValue < 0) {
+    return 'На границах интервала не могут быть отрицательные числа';
   }
-  return 'Функция работает только для целых положительных чисел, а также первое число должно быть строго меньше второго.';
+  return (Math.round((Math.random() * INTEGER_RANGE_OF_NUMBERS) + Math.ceil(minValue)).toFixed(0));
 }
 
-generateRandomNumber(10, 24);
+generateRandomIntegerNumberFromInterval(10, 24);
 
-function generateRandomDecimalNumber(minValue, maxValue,decimalPlaces){
-  if (minValue >= 0 && minValue.toFixed(decimalPlaces) < maxValue.toFixed(decimalPlaces)){
-    return  (Math.fround((Math.random( ) * (maxValue - minValue)) + minValue)).toFixed(decimalPlaces);
+function generateRandomDecimalNumberFromInterval(minValue, maxValue, decimalPlaces) {
+  const RANGE_OF_NUMBERS = maxValue - minValue;
+  const MULTIPLYER_FOR_VALUE_CUTTING = Math.pow(10, decimalPlaces);
+  const RANGE_OF_NUMBERS_WITH_DECIMAL_PLACES_GRANTING = (Math.floor(maxValue*MULTIPLYER_FOR_VALUE_CUTTING))/MULTIPLYER_FOR_VALUE_CUTTING - (Math.ceil(minValue*MULTIPLYER_FOR_VALUE_CUTTING))/MULTIPLYER_FOR_VALUE_CUTTING;
+  if (typeof minValue !== 'number' && typeof minValue !== 'number' && typeof decimalPlaces !== 'number') {
+    return 'Аргументы функции должны быть числами';
+  } else if (RANGE_OF_NUMBERS <= 0) {
+    return 'Меньшее число должно быть меньше большего числа';
+  } else if (minValue < 0) {
+    return 'На границах интервала не могут быть отрицательные числа';
+  } else if (!Number.isInteger(decimalPlaces)) {
+    return 'Количество знаков после запятой не может быть дробным числом';
+  } else if (decimalPlaces < 0) {
+    return 'Количество знаков после запятой не может быть отрицательным числом';
+  } else if (RANGE_OF_NUMBERS_WITH_DECIMAL_PLACES_GRANTING < 0) {
+    return 'В указанном интервале отсутствуют числа с заданным количеством знаков после запятой';
   }
-  return 'Функция работает только для положительных чисел, а также первое число должно быть строго меньше второго в указанном десятичном диапазоне значений.';
+  return (Math.fround((Math.random() * RANGE_OF_NUMBERS_WITH_DECIMAL_PLACES_GRANTING) + (Math.ceil(minValue*MULTIPLYER_FOR_VALUE_CUTTING))/MULTIPLYER_FOR_VALUE_CUTTING)).toFixed(decimalPlaces);
 }
 
-generateRandomDecimalNumber(0.025, 0.026, 4);
+generateRandomDecimalNumberFromInterval(0.025, 0.026);
+
