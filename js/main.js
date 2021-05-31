fcd03d58fc8c98c5e5ffc8996a047e435924fc13
DruidNoname <oboro.msu@gmail.com>
2021-05-31 14:09:51 +0300

    АÐшÑFixed both random-generated functions

--- js/main.js
+++ js/main.js
@@ -1,41 +1,54 @@
 // источник - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 function generateRandomIntegerNumberFromInterval(minValue, maxValue) {
-  const RANGE_OF_NUMBERS = maxValue - minValue;
-  const INTEGER_RANGE_OF_NUMBERS = Math.floor(maxValue) - Math.ceil(minValue);
-  if (typeof minValue !== 'number' && typeof minValue !== 'number') {
+  const rangeOfNumbers = maxValue - minValue;
+  const integerRangeOfNumbers = Math.floor(maxValue) - Math.ceil(minValue);
+  const randomArea = Math.random() * (integerRangeOfNumbers + 1) + Math.ceil(minValue);
+  if (typeof minValue !== 'number' || typeof maxValue !== 'number') {
     return 'Аргументы функции должны быть числами';
-  } else if (RANGE_OF_NUMBERS <= 0) {
-    return 'Меньшее число должно быть меньше большего числа';
-  } else if (INTEGER_RANGE_OF_NUMBERS < 0) {
+  }
+  if (rangeOfNumbers <= 0) {
+    return 'Некорректный интервал';
+  }
+  if (integerRangeOfNumbers < 0) {
     return 'В указанном интервале отсутствуют целые числа';
-  } else if (minValue < 0) {
+  }
+  if (minValue < 0) {
     return 'На границах интервала не могут быть отрицательные числа';
   }
-  return (Math.round((Math.random() * INTEGER_RANGE_OF_NUMBERS) + Math.ceil(minValue)).toFixed(0));
+  return Math.floor(randomArea);
 }
 
 generateRandomIntegerNumberFromInterval(10, 24);
 
 function generateRandomDecimalNumberFromInterval(minValue, maxValue, decimalPlaces) {
-  const RANGE_OF_NUMBERS = maxValue - minValue;
-  const MULTIPLYER_FOR_VALUE_CUTTING = Math.pow(10, decimalPlaces);
-  const RANGE_OF_NUMBERS_WITH_DECIMAL_PLACES_GRANTING = (Math.floor(maxValue*MULTIPLYER_FOR_VALUE_CUTTING))/MULTIPLYER_FOR_VALUE_CUTTING - (Math.ceil(minValue*MULTIPLYER_FOR_VALUE_CUTTING))/MULTIPLYER_FOR_VALUE_CUTTING;
-  if (typeof minValue !== 'number' && typeof minValue !== 'number' && typeof decimalPlaces !== 'number') {
+  const rangeOfNumbers = maxValue - minValue;
+  const multiplyerForValueCutting = Math.pow(10, decimalPlaces);
+  const maxValueRounded = Math.floor(maxValue * multiplyerForValueCutting) / multiplyerForValueCutting;
+  const minValueRounded = Math.ceil(minValue * multiplyerForValueCutting) / multiplyerForValueCutting;
+  const rangeOfNumbersWithDecimal = maxValueRounded - minValueRounded;
+  const randomArea = Math.random() * (rangeOfNumbersWithDecimal + (1 / multiplyerForValueCutting)) + minValueRounded;
+  if (typeof minValue !== 'number' || typeof maxValue !== 'number' || typeof decimalPlaces !== 'number') {
     return 'Аргументы функции должны быть числами';
-  } else if (RANGE_OF_NUMBERS <= 0) {
-    return 'Меньшее число должно быть меньше большего числа';
-  } else if (minValue < 0) {
+  }
+  if (rangeOfNumbers <= 0) {
+    return 'Некорректный интервал';
+  }
+  if (minValue < 0) {
     return 'На границах интервала не могут быть отрицательные числа';
-  } else if (!Number.isInteger(decimalPlaces)) {
+  }
+  if (!Number.isInteger(decimalPlaces)) {
     return 'Количество знаков после запятой не может быть дробным числом';
-  } else if (decimalPlaces < 0) {
+  }
+  if (decimalPlaces < 0) {
     return 'Количество знаков после запятой не может быть отрицательным числом';
-  } else if (RANGE_OF_NUMBERS_WITH_DECIMAL_PLACES_GRANTING < 0) {
+  }
+  if (rangeOfNumbersWithDecimal < 0) {
     return 'В указанном интервале отсутствуют числа с заданным количеством знаков после запятой';
   }
-  return (Math.fround((Math.random() * RANGE_OF_NUMBERS_WITH_DECIMAL_PLACES_GRANTING) + (Math.ceil(minValue*MULTIPLYER_FOR_VALUE_CUTTING))/MULTIPLYER_FOR_VALUE_CUTTING)).toFixed(decimalPlaces);
+  return Number((Math.floor(randomArea * multiplyerForValueCutting) / multiplyerForValueCutting).toFixed(decimalPlaces));
 }
 
-generateRandomDecimalNumberFromInterval(0.025, 0.026);
-
+generateRandomDecimalNumberFromInterval(0.025, 0.026, 4);
+generateRandomDecimalNumberFromInterval(1.026, 1.026, 4);
+generateRandomDecimalNumberFromInterval(1.026, 'селёдка', 4);
