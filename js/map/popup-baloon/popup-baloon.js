08c16ffa432c622efc98eed3b4addbf98431dca9
DruidNoname <oboro.msu@gmail.com>
2021-06-19 20:25:09 +0300

    function popup balloon creating tier 7

--- js/map/popup-baloon/popup-baloon.js
+++ js/map/popup-baloon/popup-baloon.js
@@ -15,15 +15,16 @@ const balloons = createRandomOffers(3);
 function setTypeOfQuarters(offerData, template) {
   for (const key in QUARTERS_TYPES_WITH_NAMES) {
     if (key === offerData.offer.type) {
-      template.querySelector('.popup__type').textContent = QUARTERS_TYPES_WITH_NAMES[key]; //required by considerations
+      template.querySelector('.popup__type').textContent = QUARTERS_TYPES_WITH_NAMES[key];
     }
   }
 }
 
 function setFeatures(offerData, template) {
   const featuresTemplate = template.querySelector('.popup__features');
+
   if (offerData.offer.features !== undefined && offerData.offer.features.length !== 0) {
-    featuresTemplate.textContent = offerData.offer.features.join(', '); //not marked as required
+    featuresTemplate.textContent = offerData.offer.features.join(', ');
   } else {
     featuresTemplate.remove();
   }
@@ -31,8 +32,9 @@ function setFeatures(offerData, template) {
 
 function setDescription(offerData, template) {
   const descriptionTemplate = template.querySelector('.popup__description');
+
   if (offerData.offer.description !== undefined && offerData.offer.description.length !== 0) {
-    descriptionTemplate.textContent = offerData.offer.description; //not marked as required
+    descriptionTemplate.textContent = offerData.offer.description;
   } else {
     descriptionTemplate.remove();
   }
@@ -41,11 +43,12 @@ function setDescription(offerData, template) {
 function setPhotos(offerData, template) {
   const popupPhotoBlock = template.querySelector('.popup__photos');
   const photoTemplate = popupPhotoBlock.querySelector('.popup__photo');
+
   if (offerData.offer.photos !== undefined && offerData.offer.photos.length !== 0) {
     offerData.offer.photos.forEach((photoLink) => {
       const popupPhoto = photoTemplate.cloneNode(true);
       popupPhoto.setAttribute('src', photoLink);
-      popupPhotoBlock.appendChild(popupPhoto); //not marked as required
+      popupPhotoBlock.appendChild(popupPhoto);
     });
     popupPhotoBlock.removeChild(photoTemplate);
   } else {
@@ -55,8 +58,9 @@ function setPhotos(offerData, template) {
 
 function setAvatar(offerData, template) {
   const avatarTemplate = template.querySelector('.popup__avatar');
+
   if (offerData.author.avatar !== undefined && offerData.author.avatar.length !== 0) {
-    avatarTemplate.setAttribute('src', offerData.author.avatar); //not marked as required
+    avatarTemplate.setAttribute('src', offerData.author.avatar);
   } else {
     avatarTemplate.remove();
   }
@@ -65,16 +69,19 @@ function setAvatar(offerData, template) {
 function drawBalloons() {
   balloons.forEach((offerData) => {
     const popupBalloon = popupBalloonTemplate.cloneNode(true);
-    popupBalloon.querySelector('.popup__title').textContent = offerData.offer.title; //required
-    popupBalloon.querySelector('.popup__text--address').textContent = offerData.offer.address; //required, after loading
-    popupBalloon.querySelector('.popup__text--price').textContent = offerData.offer.price; //required
-    popupBalloon.querySelector('.popup__text--capacity').textContent = `${offerData.offer.rooms} комнат для ${offerData.offer.guests} гостей`; //required by considerations
-    popupBalloon.querySelector('.popup__text--time').textContent = `Заезд после ${offerData.offer.checkin}, выезд до ${offerData.offer.checkout}`; //required by considerations
+
+    popupBalloon.querySelector('.popup__title').textContent = offerData.offer.title;
+    popupBalloon.querySelector('.popup__text--address').textContent = offerData.offer.address;
+    popupBalloon.querySelector('.popup__text--price').textContent = offerData.offer.price;
+    popupBalloon.querySelector('.popup__text--capacity').textContent = `${offerData.offer.rooms} комнат для ${offerData.offer.guests} гостей`;
+    popupBalloon.querySelector('.popup__text--time').textContent = `Заезд после ${offerData.offer.checkin}, выезд до ${offerData.offer.checkout}`;
+
     setTypeOfQuarters(offerData, popupBalloon);
     setFeatures(offerData, popupBalloon);
     setDescription(offerData, popupBalloon);
     setPhotos(offerData, popupBalloon);
     setAvatar(offerData, popupBalloon);
+
     map.appendChild(popupBalloon);
   });
 }
